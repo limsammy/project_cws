89d0811fb4fc4452667c0eb61d0ddf2c0aeaa781
rorong <rordev@ongraph.com>
2017-09-22 10:41:53 +0530

    fax issue solved

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170921070306) do
+ActiveRecord::Schema.define(version: 20170922045723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 20170921070306) do
     t.string "name"
     t.string "department"
     t.string "phone_number"
-    t.integer "fax"
+    t.string "fax"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

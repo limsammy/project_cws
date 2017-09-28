eda3df62bf5552df1d1eb1555b43dbf85234b1b4
Sam Lim <limsammy1@gmail.com>
2017-09-28 00:33:25 -0700

    added migration

--- db/migrate/20170928072718_add_profile_image_to_users.rb
+++ db/migrate/20170928072718_add_profile_image_to_users.rb
@@ -1,4 +1,11 @@
 class AddProfileImageToUsers < ActiveRecord::Migration[5.1]
   def change
+    def self.up
+      add_column :users, :profile_image, :binary, :limit => 5.megabyte
+    end
+
+    def self.down
+      remove_column :users, :profile_image
+    end
   end
 end

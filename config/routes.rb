54012fb494520a1a4577fe23bfe90bce19a2a12a
rorong <rordev@ongraph.com>
2017-09-28 16:14:13 +0530

    user profile update

--- config/routes.rb
+++ config/routes.rb
@@ -45,5 +45,6 @@ Rails.application.routes.draw do
 
   resources :orders do
   end
+  resources :profiles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

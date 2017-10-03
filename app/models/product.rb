a29bc2de0a713782aba9473bed624abb54dad2cd
rorong <rordev@ongraph.com>
2017-10-03 11:55:42 +0530

    relationship between category and product done

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,5 @@ class Product < ApplicationRecord
 	accepts_nested_attributes_for :order_items,  allow_destroy: true
   validates :name, :price, :stock_in_hand, presence: true
   validates :code, presence: true, uniqueness: true
+  belongs_to :category
 end
--- db/migrate/20171003044246_rename_company_id_in_contact.rb
+++
@@ -1,5 +0,0 @@
-class RenameCompanyIdInContact < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :contacts, :company_id, :customer_id
-  end
-end

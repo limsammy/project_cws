ea387653f0851752e9c17b201ed9207fe6d1c7bf
rorong <rordev@ongraph.com>
2017-10-09 12:03:34 +0530

    changes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,29 +1,22 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
-  # GET /products
-  # GET /products.json
   def index
     @products = Product.all
   end
 
-  # GET /products/1
-  # GET /products/1.json
   def show
   end
 
-  # GET /products/new
   def new
     @product = Product.new
     @product.order_items.build
   end
 
-  # GET /products/1/edit
   def edit
   end
 
-  # POST /products
-  # POST /products.json
+
   def create
     @product = Product.new(product_params)
     respond_to do |format|
@@ -37,8 +30,6 @@ class ProductsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /products/1
-  # PATCH/PUT /products/1.json
   def update
     respond_to do |format|
       if @product.update(product_params)

17a220be8dc64cf38124c50e42b40513a04bac29
limsammy1@gmail.com <limsammy1@gmail.com>
2017-10-13 02:13:27 -0700

    Good seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,8 @@
 include ApplicationHelper
 
 if User.count == 0
-  10.times do
+  10.times do |f|
+  puts "Creating user #{f}"
     user = User.create!(
       first_name: Faker::Name.first_name,
       last_name: Faker::Name.last_name,
@@ -28,7 +29,8 @@ if User.count == 0
 end
 
 if Customer.count == 0
-  10.times do
+  10.times do |f|
+  puts "Creating Customer #{f}"
     Customer.create!(
       name: Faker::Company.name,
       phone_number: Faker::Number.number(10),
@@ -37,8 +39,10 @@ if Customer.count == 0
     )
   end
 end
+
 if Category.count == 0
-  10.times do
+  10.times do |f|
+  puts "Creating category #{f}"
     Category.create!(
       name: Faker::Dessert.variety,
       item_code: Faker::Number.number(10),
@@ -47,9 +51,11 @@ if Category.count == 0
     )
   end
 end
+
 if Product.count == 0
   Category.all.each do |category|
-    10.times do
+    10.times do |f|
+      puts "Creating category for product #{f}"
       Product.create!(
         name: Faker::Food.dish,
         price: Faker::Number.decimal(2),
@@ -62,12 +68,14 @@ if Product.count == 0
 end
 
 if Contact.count == 0
-  10.times do
+  10.times do |f|
+    puts "Creating contact#{f}"
     contact=Contact.create!(
       name: Faker::Name.first_name,
       email: Faker::Internet.email,
       mob_number: Faker::Number.number(10),
-      customer_id: Customer.first.id
+      customer_id: Customer.first.id,
+      created_at: (rand*10).days.ago
     )
     contact.address = Address.create!(
       line_1: Faker::Address.street_name,
@@ -81,13 +89,20 @@ if Contact.count == 0
 end
 
 if Order.count == 0
-  10.times do
+  10.times do |f|
+    puts "Creating order #{f}"
+    id = Contact.find(rand(Contact.count)).id
+    if id == 0
+      id += 1
+    end
     order = Order.create!(
       order_number: order_number,
       description: Faker::Commerce.product_name,
       customer_id: Customer.first.id,
-      contact_id: Contact.first.id,
-      ordered_at: DateTime.now
+      created_at: (rand*10).days.ago,
+      ordered_at: (rand*10).days.ago,
+      updated_at: (rand*10).days.ago,
+      contact_id: id
     )
     order.shipping_address = Address.create!(
       line_1: Faker::Address.street_name,
@@ -95,21 +110,31 @@ if Order.count == 0
       country: Faker::Address.country,
       state: Faker::Address.state,
       zip: Faker::Address.zip,
-      city: Faker::Address.city
+      city: Faker::Address.city,
+      created_at: (rand*10).days.ago,
+      updated_at: (rand*10).days.ago
     )
   end
 end
 
 if OrderItem.count == 0
   Order.all.each do |order|
-    10.times do
+    10.times do |f|
+      puts "Creating orderitem #{f}"
       OrderItem.create!(
         quantity: 1,
         order_id: order.id,
         product_id: Product.first.id,
         amount: Product.first.price * 1,
-        unit_price: Product.first.price
+        unit_price: Product.first.price,
+        created_at: (rand*10).days.ago,
+        updated_at: (rand*10).days.ago
       )
     end
   end
+end
+
+if !User.find_by(email: 'admin@admin.com')
+  puts "Creating admin"
+  User.create!(email: 'admin@admin.com', password: 'password')
 end
